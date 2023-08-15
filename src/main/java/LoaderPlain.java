91a048722ff73bc79b4beb2d70043c8618199c54
四月櫻花 洇染成殤 <102905510+nuym@users.noreply.github.com>
2023-08-15 12:42:28 +0800

    Delete src/main/java directory

--- src/main/java/LoaderPlain.java
+++
@@ -1,8 +0,0 @@
-public class LoaderPlain {
-    public static void registerNativesForClass(int index, LoaderHelper clazz) {
-    }
-
-    static {
-        System.loadLibrary("%LIB_NAME%");
-    }
-}
