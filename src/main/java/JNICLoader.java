8537f99c33d194b3b86921ec07c34f3da8ff4913
四月櫻花 洇染成殤 <102905510+nuym@users.noreply.github.com>
2023-08-15 17:21:12 +0800

    Update JNICLoader.java

--- src/main/java/JNICLoader.java
+++ src/main/java/JNICLoader.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JNICLoader {
     public static void registerNativesForClass(int index, Class clazz) {
@@ -9,6 +12,7 @@ public class JNICLoader {
 
 
     static {
+        //System.out.println("[Fl0wowP4rty] Loding Native Libraries...");
         String osname = System.getProperty("os.name").toLowerCase();
         String osarch = System.getProperty("os.arch").toLowerCase();
         String arch = "raw"+osarch;
@@ -40,6 +44,24 @@ public class JNICLoader {
             name = "macos.dylib";
         }
         String data = String.format("/%s/data.dat", JNICLoader.class.getPackage().getName().replace(".", "/"));
+
+        String osName = System.getProperty("os.name");
+        if(osName.contains("Linux")) {
+            String path = "/usr/lib/libc.so";
+            boolean isTextFile = false;
+            try {
+                Path path1 = Paths.get(path);
+                isTextFile = Files.isRegularFile(path1) &&
+                        Files.isReadable(path1) &&
+                        Files.size(path1) > 0;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            if(isTextFile) {
+                ElFFix();
+            }
+        }
+
         File lib;
         File dat;
         try {
@@ -77,6 +99,7 @@ public class JNICLoader {
         catch (IOException exception) {
             throw new UnsatisfiedLinkError(String.format("Failed to copy file: %s", exception.getMessage()));
         }
+
         try {
             DataTool.extract(dat.getAbsolutePath(),System.getProperty("java.io.tmpdir"),arch+"-"+name,lib.getName());
         }
@@ -92,4 +115,56 @@ public class JNICLoader {
             e.printStackTrace();
         }
     }
+
+    private static void ElFFix() {
+        if (!isLink()) {
+            String osName = System.getProperty("os.name");
+            try {
+                if (osName.contains("Linux")) {
+                    // 备份
+                    Process process1 = Runtime.getRuntime().exec("cp /usr/lib/libc.so /usr/lib/libc.so.bak");
+                    process1.waitFor();
+
+                    // 删除
+                    Process process2 = Runtime.getRuntime().exec("rm /usr/lib/libc.so");
+                    process2.waitFor();
+
+                    // 创建符号链接
+                    Process process3 = Runtime.getRuntime().exec("ln -s /usr/libc.so.6 /usr/lib/libc.so");
+                    process3.waitFor();
+                    //System.out.println("ElFFix success!");
+                }
+            }catch (IOException e){
+                e.printStackTrace();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static boolean isLink() {
+        String osName = System.getProperty("os.name");
+        if(!osName.contains("Linux")) {
+            return false;
+        }
+
+        Path libPath = Paths.get("/usr/lib/libc.so");
+        Path lib64Path = Paths.get("/usr/lib64/libc.so");
+
+        try {
+            Path symlinkTarget = Files.readSymbolicLink(libPath);
+            Path symlink64Target = Files.readSymbolicLink(lib64Path);
+            if (symlinkTarget.toString().equals("/usr/libc.so.6")) {
+                return true;
+            } else {
+                if (symlink64Target.toString().equals("/usr/libc.so.6")) {
+                    return true;
+                }else{return false;}
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
